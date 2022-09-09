c93d8b6f6ad5d48da468431923dc8650ca1e7f75
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-09-09 19:16:44 +0600

    Arrow Function With Parameters

---
+++ arrowFunctionWithParameters.js
@@ -0,0 +1,13 @@
+const myConcat = (arr1, arr2) => {
+  return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
+
+const doubler = (item) => item * 2;
+doubler(4);
+
+const doubler2 = item => item * 2;
+
+const multiplier = (item, multi) => item * multi;
+multiplier(4, 2);
