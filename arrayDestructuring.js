dbd4e5ad98135b566edbbc9522e1bf5399f1dbd1
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-09-10 11:07:58 +0600

    Destructuring from Array

---
+++ arrayDestructuring.js
@@ -0,0 +1,13 @@
+// 1
+let a = 8, b = 6;
+// Only change code below this line
+
+[a, b] = [b, a];
+
+// 2
+// const [a, b] = [1, 2, 3, 4, 5, 6];
+// console.log(a, b);
+
+// 3
+// const [a, b,,, c] = [1, 2, 3, 4, 5, 6];
+// console.log(a, b, c);
